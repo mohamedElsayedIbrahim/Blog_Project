02a3e859628124fe309efad6e3e3e1951b23d0e5
mohamedElsayedIbrahim <mohamdeesayed@outlook.com>
2023-07-16 16:34:39 +0300

    first commit

---
+++ login.php
@@ -0,0 +1,33 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+</head>
+<body>
+   <div class="w-75 m-auto py-5">
+    <div class="text-center">
+        <img width="200" src="./img/PHP_logo.png" alt="php logo">
+    </div>
+    <h1 class="my-3">Login to system....</h1>
+
+    <form>
+        <div class="mb-3">
+            <label for="username">Username</label>
+            <input type="text" class="form-control" id="username">
+        </div>
+        <div class="mb-3">
+            <label for="password">password</label>
+            <input type="text" class="form-control" id="password">
+        </div>
+        <div class="mb-3 text-center">
+            <button class="btn btn-danger">Login system</button>
+        </div>
+    </form>
+   </div>
+
+    <script src="./js/bootstrap.bundle.js"></script>
+</body>
+</html>
